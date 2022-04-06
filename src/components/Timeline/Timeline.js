7c00d0ff81d468ad80affa8158a3114fe662f4be
ClockWize171 <jupiterminhtin998@gmail.com>
2022-04-06 23:33:59 +0630

    adjust timeline data

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -48,7 +48,7 @@ const Timeline = () => {
                         whileInView={{ x: [100, 0], opacity: [0, 1] }}
                         className="timeline-content">
                         <h3>Info Myanmar University</h3>
-                        <strong>2017 - 2019</strong>
+                        {isNotSmallerScreen ? <strong>2019 - 2017</strong> : <strong>2017 - 2019</strong>}
                     </motion.div>
                 </div>
 
@@ -76,7 +76,7 @@ const Timeline = () => {
                         whileInView={{ x: [100, 0], opacity: [0, 1] }}
                         className="timeline-content">
                         <h3>B.Sc (Hons) Computing, Edinburgh Napier University </h3>
-                        <strong>2020 - Present</strong>
+                        {isNotSmallerScreen ? <strong>2022 - 2020</strong> : <strong>2020 - 2022</strong>}
                     </motion.div>
                 </div>
 
